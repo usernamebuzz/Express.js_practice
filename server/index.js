2eeed9c39a7a1ea8e2de09900f3f50f5359f60b9
usernamebuzz <ohminji@ewhain.net>
2023-10-07 21:03:22 +0900

    Feat: Route page

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ app.use(express.json());
 
 const db = require("./models");
 
+// Routers
+const postRouter = require("./routes/posts");
+app.use("/posts", postRouter);
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
