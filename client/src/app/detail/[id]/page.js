17a6bf10d7ee46a1177a86311c9ab91579cf0bde
usernamebuzz <ohminji@ewhain.net>
2023-10-09 12:59:35 +0900

    Feat: Fetch coments

--- client/src/app/detail/[id]/page.js
+++ client/src/app/detail/[id]/page.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import styles from './page.module.css';
 
 export default function Post({ params }) {
   async function fetchPost() {
@@ -11,21 +12,51 @@ export default function Post({ params }) {
     return response.data;
   }
 
-  const { data: post, error, isLoading } = useQuery(['post'], fetchPost);
+  async function fetchComments() {
+    const response = await axios.get(
+      `http://localhost:3001/comments/${params.id}`
+    );
+    return response.data;
+  }
 
-  if (isLoading) {
+  const {
+    data: post,
+    error: postError,
+    isLoading: postIsLoading,
+  } = useQuery(['post'], fetchPost);
+
+  const {
+    data: comments,
+    error: commentsError,
+    isLoading: commentsIsLoading,
+  } = useQuery(['comments', params.id], fetchComments);
+
+  console.log(comments);
+
+  if (postIsLoading || commentsIsLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  if (postError) {
+    return <div>Error fetching post: {postError.message}</div>;
+  }
+
+  if (commentsError) {
+    return <div>Error fetching comments: {commentsError.message}</div>;
   }
 
   return (
-    <div className="post" key={post.id}>
-      <div className="title"> {post.title} </div>
-      <div className="body">{post.postText}</div>
-      <div className="footer">{post.username}</div>
-    </div>
+    <>
+      <div className={styles.post}>
+        <div className={styles.title}> {post.title} </div>
+        <div className={styles.body}>{post.postText}</div>
+        <div className={styles.footer}>{post.username}</div>
+      </div>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>{comment.commentBody}</li>
+        ))}
+      </ul>
+    </>
   );
 }
